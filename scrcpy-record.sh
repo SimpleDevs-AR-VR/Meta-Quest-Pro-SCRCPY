4ab3084e46dc0df84fae7ec9950e20706fd3cf2e
TORRENS_PHD_ACCOUNT <TORRENS_PHD_ACCOUNT@NYU>
2024-01-21 20:28:07 -0500

    updated scrcpy-record.sh with new version of filenaming that enables for unix timestamping

--- scrcpy-record.sh
+++ scrcpy-record.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-current_date_time = $(date +"%Y-%m-%d_%T")
-scrcpy -m1080 --record "/home/torrens-phd-account/Desktop/scrcpy-recordings/$(date +"%Y-%m-%d_%T").mp4"
+scrcpy -m1080 --record "/home/torrens-phd-account/Desktop/scrcpy-recordings/$(date +"%Y-%m-%d_%H-%M-%S")_$(date +%s%N | cut -b1-13).mp4"
